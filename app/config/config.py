c18d9fbdad3b0d60ae41e576d21d32ae4355f5a9
MarcosCastelo <marcosvitorcastelo@gmail.com>
2025-08-06 22:48:50 -0300

    deploy configs

--- app/config/config.py
+++ app/config/config.py
@@ -8,30 +8,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///local.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
-    
-    @staticmethod
-    def init_app(app):
-        pass
-
-class ProductionConfig(Config):
-    DEBUG = False
-    # Para Heroku, ajustar DATABASE_URL se necessário
-    @classmethod
-    def init_app(cls, app):
-        Config.init_app(app)
-        
-        # Log para stderr
-        import logging
-        from logging import StreamHandler
-        file_handler = StreamHandler()
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-
-config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
-}
